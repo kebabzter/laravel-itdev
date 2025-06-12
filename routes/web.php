51a0c65f09c9e62233948b4b9636ff4f5ed77fd1
kebabzter <gabzterthesecond@gmail.com>
2025-06-12 16:01:26 +0200

    Changes to user access permission

--- routes/web.php
+++ routes/web.php
@@ -8,20 +8,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/examples', [ExampleController::class, 'index'])->name('examples.index');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/examples', [ExampleController::class, 'index'])->name('examples.index');
+
+    // Routes dashboard
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    // In routes/web.php
+    Route::post('/examples', [ExampleController::class, 'store'])->name('example.store');
+    Route::delete('/examples/{example}', [ExampleController::class, 'destroy'])->name('example.destroy');
 });
 
-// In routes/web.php
-Route::post('/examples', [ExampleController::class, 'store'])->name('example.store');
-Route::delete('/examples/{example}', [ExampleController::class, 'destroy'])->name('example.destroy');
 
 require __DIR__.'/auth.php';
